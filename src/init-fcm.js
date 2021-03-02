8a6aab8e1f62e540813e88e176c2d0261586c18d
Akshayfpz <47168065+Akshayfpz@users.noreply.github.com>
2021-03-02 18:16:25 +0530

    Update init-fcm.js

--- src/init-fcm.js
+++ src/init-fcm.js
@@ -4,13 +4,13 @@ import "firebase/database";
 
 const initializedFirebaseApp = firebase.initializeApp({
      // Project Settings => Add Firebase to your web app
-	 apiKey: "AIzaSyBgxl_OAn5SHDyRD4ijn6aKAIkHNmQBh78",
-	 authDomain: "angular-fc747.firebaseapp.com",
-	 databaseURL: "https://angular-fc747.firebaseio.com",
-	 projectId: "angular-fc747",
-	 storageBucket: "angular-fc747.appspot.com",
-	 messagingSenderId: "299138396800",
-	 appId: "1:299138396800:web:c0c9ea30919d4f50"
+	 apiKey: "xxxxxx",
+	 authDomain: "xxxxx",
+	 databaseURL: "xxxxxx",
+	 projectId: "xxxxx",
+	 storageBucket: "xxxxx",
+	 messagingSenderId: "xxxxxxx",
+	 appId: "xxxxxxx"
 });
 const messaging = initializedFirebaseApp.messaging();
 
